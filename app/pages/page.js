859e17b47dddfb0372d2fe28cd30f97fa47242cb
Musabbir <musabbir.net@gmail.com>
2024-05-31 17:19:18 +0600

    another page MainHeader add

--- app/pages/page.js
+++ app/pages/page.js
@@ -1,10 +1,14 @@
+import MainHeader from "../components/MainHeader";
+
 export default function page() {
-    return (
+  return (
+    <div>
+      <MainHeader />
       <div className="w-full flex items-center justify-center h-screen">
         <div className="flex items-center gap-3 text-3xl">
           <h1 className="text-3xl font-bold italic">Comming Soon </h1>🥰
         </div>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
